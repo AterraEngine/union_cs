2b09053e3fabb109fa5ccc16b8d2bf33deee747e
AndreasSasDev <directive.athena@gmail.com>
2024-11-21 22:10:58 +0100

    Feat: Implicits for SuccessOrFailure

--- src/AterraEngine.Unions/Unions/SuccessOrFailure.cs
+++ src/AterraEngine.Unions/Unions/SuccessOrFailure.cs
@@ -6,4 +6,7 @@ namespace AterraEngine.Unions;
 // ---------------------------------------------------------------------------------------------------------------------
 // Code
 // ---------------------------------------------------------------------------------------------------------------------
-public readonly partial struct SuccessOrFailure<TSuccess, TFailure>() : IUnion<Success<TSuccess>, Failure<TFailure>>;
+public readonly partial struct SuccessOrFailure<TSuccess, TFailure>() : IUnion<Success<TSuccess>, Failure<TFailure>> {
+    public static implicit operator SuccessOrFailure<TSuccess, TFailure>(TSuccess value) => new Success<TSuccess>(value);
+    public static implicit operator SuccessOrFailure<TSuccess, TFailure>(TFailure value) => new Failure<TFailure>(value);
+}
