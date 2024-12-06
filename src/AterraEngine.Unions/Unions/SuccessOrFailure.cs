82d1f520c66082a43dbc65b5c665cdc8e0563df1
Anna Sas <anna.sas.dev@gmail.com>
2024-12-06 15:52:45 +0100

    Fix: Correctly working Bump command

--- src/AterraEngine.Unions/Unions/SuccessOrFailure.cs
+++ src/AterraEngine.Unions/Unions/SuccessOrFailure.cs
@@ -28,3 +28,38 @@ public readonly partial struct SuccessOrFailure<TSuccess, TFailure>() : IUnion<S
         return value is not null;
     }
 }
+
+[UnionAliases(null, "Failure")]
+public readonly partial struct SuccessOrFailure<TSuccess>() : IUnion<Success<TSuccess>, Failure<string>> {
+    public static implicit operator SuccessOrFailure<TSuccess>(TSuccess value) => new Success<TSuccess>(value);
+    public static implicit operator SuccessOrFailure<TSuccess>(string value) => new Failure<string>(value);
+    
+    public bool TryGetAsSuccessValue([NotNullWhen(true)] out TSuccess? value) {
+        value = default;
+        if (!IsSuccess) return false;
+
+        value = AsSuccess.Value;
+        return value is not null;
+    }
+    
+    public bool TryGetAsFailureValue([NotNullWhen(true)] out string? value) {
+        value = default;
+        if (!IsFailure) return false;
+
+        value = AsFailure.Value;
+        return true;
+    }
+}
+
+[UnionAliases(null, "Failure")]
+public readonly partial struct SuccessOrFailure() : IUnion<Success, Failure<string>> {
+    public static implicit operator SuccessOrFailure(string value) => new Failure<string>(value);
+    
+    public bool TryGetAsFailureValue([NotNullWhen(true)] out string? value) {
+        value = default;
+        if (!IsFailure) return false;
+
+        value = AsFailure.Value;
+        return true;
+    }
+}
--- tools/AterraEngine.Unions.Tools/Commands/VersionBumpCommands.cs
+++ tools/AterraEngine.Unions.Tools/Commands/VersionBumpCommands.cs
@@ -2,10 +2,11 @@
 // Imports
 // ---------------------------------------------------------------------------------------------------------------------
 using CliArgsParser;
+using System.Diagnostics;
+using System.Xml;
 using System.Xml.Linq;
 
 namespace AterraEngine.Unions.Tools.Commands;
-
 // ---------------------------------------------------------------------------------------------------------------------
 // Code
 // ---------------------------------------------------------------------------------------------------------------------
@@ -13,36 +14,95 @@ public class VersionBumpCommands : ICommandAtlas {
 
     [Command<VersionBumpParameters>("bump")]
     public async Task VersionBumpCommand(VersionBumpParameters args) {
-        string[] projectFiles = ["AterraEngine.Unions.csproj", "AterraEngine.Unions.Generators.csproj"];
-        VersionSection sectionToBump = args.Section;
+        Console.WriteLine("Bumping version...");
+        SuccessOrFailure<string> bumpResult = await BumpVersion(args);
+        if (bumpResult is { IsFailure: true, AsFailure.Value: var errorBumping }) {
+            Console.WriteLine(errorBumping);
+            return;
+        }
+        
+        string updatedVersion = bumpResult.AsSuccess.Value;
+        
+        Console.WriteLine("Git committing ...");
+        SuccessOrFailure gitCommitResult = await TryCreateGitCommit(updatedVersion);
+        if (gitCommitResult is { IsFailure: true, AsFailure.Value: var errorCommiting }) {
+            Console.WriteLine(errorCommiting);
+            return;
+        }
+        
+        Console.WriteLine("Git tagging ...");
+        SuccessOrFailure gitTagResult = await TryCreateGitTag(updatedVersion);
+        if (gitTagResult is { IsFailure: true, AsFailure.Value: var errorTagging }) {
+            Console.WriteLine(errorTagging);
+            return;
+        }
+        
+        Console.WriteLine($"Version {updatedVersion} committed and tagged successfully.");
+    }
+    
+    private async static Task<SuccessOrFailure> TryCreateGitTag(string updatedVersion) {
+        var gitTagInfo = new ProcessStartInfo("git", "tag v" + updatedVersion) {
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
 
-        foreach (string projectFile in projectFiles)
-        {
+        using Process? gitTagProcess = Process.Start(gitTagInfo);
+        Console.WriteLine(await gitTagProcess?.StandardOutput.ReadToEndAsync()!);
+        await gitTagProcess.WaitForExitAsync();
+        
+        if (gitTagProcess.ExitCode == 0) return "Git Tagging failed";
+        return new Success();
+
+    }
+    private async static Task<SuccessOrFailure> TryCreateGitCommit(string updatedVersion) {
+
+        var gitCommitInfo = new ProcessStartInfo("git", $"commit -am \"VersionBump :{updatedVersion}\"") {
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+
+        using Process? gitCommitProcess = Process.Start(gitCommitInfo);
+        Console.WriteLine(await gitCommitProcess?.StandardOutput.ReadToEndAsync()!);
+        await gitCommitProcess.WaitForExitAsync();
+        
+        if (gitCommitProcess.ExitCode == 0) return "Git Commit failed";
+        return new Success();
+    }
+
+
+    private async static Task<SuccessOrFailure<string>> BumpVersion(VersionBumpParameters args) {
+        string[] projectFiles = [
+            "src/AterraEngine.Unions/AterraEngine.Unions.csproj", 
+            "src/AterraEngine.Unions.Generators/AterraEngine.Unions.Generators.csproj"
+        ];
+        VersionSection sectionToBump = args.Section;
+        string? versionToReturn = null;
+
+        foreach (string projectFile in projectFiles) {
             string path = Path.Combine(args.Root, projectFile);
             if (!File.Exists(path)) {
-                Console.WriteLine($"Could not find {path}");
-                continue;
+                return new Failure<string>($"Could not find project file {projectFile}");
             }
 
             XDocument document;
             await using (var stream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read, 4096, true)) {
-                document = await XDocument.LoadAsync(stream, LoadOptions.None, default);
+                document = await XDocument.LoadAsync(stream, LoadOptions.PreserveWhitespace, default);
             }
-            
+
             XElement? versionElement = document
                 .Descendants("PropertyGroup")
                 .Elements("Version")
                 .FirstOrDefault();
 
             if (versionElement == null) {
-                Console.WriteLine($"Could not find version element in {path}");
-                continue;
+                return new Failure<string>($"File {projectFile} did not contain a version element");
             }
 
             string[] versionParts = versionElement.Value.Split('.');
             if (versionParts.Length != 3) {
-                Console.WriteLine($"Invalid version format: {versionElement.Value}");
-                continue;
+                return new Failure<string>($"File {projectFile} contained an invalid version element: {versionElement.Value}");
             }
 
             switch (sectionToBump) {
@@ -61,16 +121,28 @@ public class VersionBumpCommands : ICommandAtlas {
 
                 case VersionSection.None:
                 default: {
-                    Console.WriteLine($"Invalid version section: {sectionToBump}");
-                    return;
+                    return new Failure<string>($"Invalid version section {sectionToBump}");
                 }
             }
 
-            versionElement.Value = string.Join(".", versionParts);
+            versionElement.Value = versionToReturn ??= string.Join(".", versionParts);
 
-            await using (var stream = new FileStream(projectFile, FileMode.Create, FileAccess.Write, FileShare.None, 4096, true)) {
-                await document.SaveAsync(stream, SaveOptions.None, default);
+            var settings = new XmlWriterSettings {
+                Indent = true,
+                IndentChars = "    ", 
+                Async = true,
+                OmitXmlDeclaration = true,
+            };
+            await using (var stream = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.None, 4096, true)) {
+                await using var writer = XmlWriter.Create(stream, settings);
+                document.Save(writer);
             }
+            
+            Console.WriteLine($"Updated {projectFile} version to {versionElement.Value}");
         }
+
+        return versionToReturn is not null
+            ? new Success<string>(versionToReturn)
+            : new Failure<string>("Could not find a version to bump");
     }
 }
