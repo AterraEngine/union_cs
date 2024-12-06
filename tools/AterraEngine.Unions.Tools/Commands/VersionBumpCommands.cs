3d7c0608252e75a4c80fefbe147ec169a05f64a4
Anna Sas <anna.sas.dev@gmail.com>
2024-12-06 15:16:18 +0100

    Feat: Foundation for easy update tool

---
+++ tools/AterraEngine.Unions.Tools/Commands/VersionBumpCommands.cs
@@ -0,0 +1,76 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using CliArgsParser;
+using System.Xml.Linq;
+
+namespace AterraEngine.Unions.Tools.Commands;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+public class VersionBumpCommands : ICommandAtlas {
+
+    [Command<VersionBumpParameters>("bump")]
+    public async Task VersionBumpCommand(VersionBumpParameters args) {
+        string[] projectFiles = ["AterraEngine.Unions.csproj", "AterraEngine.Unions.Generators.csproj"];
+        VersionSection sectionToBump = args.Section;
+
+        foreach (string projectFile in projectFiles)
+        {
+            string path = Path.Combine(args.Root, projectFile);
+            if (!File.Exists(path)) {
+                Console.WriteLine($"Could not find {path}");
+                continue;
+            }
+
+            XDocument document;
+            await using (var stream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read, 4096, true)) {
+                document = await XDocument.LoadAsync(stream, LoadOptions.None, default);
+            }
+            
+            XElement? versionElement = document
+                .Descendants("PropertyGroup")
+                .Elements("Version")
+                .FirstOrDefault();
+
+            if (versionElement == null) {
+                Console.WriteLine($"Could not find version element in {path}");
+                continue;
+            }
+
+            string[] versionParts = versionElement.Value.Split('.');
+            if (versionParts.Length != 3) {
+                Console.WriteLine($"Invalid version format: {versionElement.Value}");
+                continue;
+            }
+
+            switch (sectionToBump) {
+                case VersionSection.Major:
+                    versionParts[0] = (int.Parse(versionParts[0]) + 1).ToString();
+                    versionParts[1] = "0";
+                    versionParts[2] = "0";
+                    break;
+                case VersionSection.Minor:
+                    versionParts[1] = (int.Parse(versionParts[1]) + 1).ToString();
+                    versionParts[2] = "0";
+                    break;
+                case VersionSection.Patch:
+                    versionParts[2] = (int.Parse(versionParts[2]) + 1).ToString();
+                    break;
+
+                case VersionSection.None:
+                default: {
+                    Console.WriteLine($"Invalid version section: {sectionToBump}");
+                    return;
+                }
+            }
+
+            versionElement.Value = string.Join(".", versionParts);
+
+            await using (var stream = new FileStream(projectFile, FileMode.Create, FileAccess.Write, FileShare.None, 4096, true)) {
+                await document.SaveAsync(stream, SaveOptions.None, default);
+            }
+        }
+    }
+}
---
+++ tools/AterraEngine.Unions.Tools/Program.cs
@@ -0,0 +1,30 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using AterraEngine.Unions.Tools.Commands;
+using CliArgsParser;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AterraEngine.Unions.Tools;
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+internal static class Program {
+    public async static Task Main(string[] args) {
+        IServiceCollection serviceCollection = new ServiceCollection();
+
+        serviceCollection.AddCliArgsParser(configuration =>
+            configuration
+                .SetConfig(new CliArgsParserConfig {
+                    Overridable = true,
+                    GenerateShortNames = true
+                })
+                .AddFromType<VersionBumpCommands>()
+        );
+
+        ServiceProvider provider = serviceCollection.BuildServiceProvider();
+
+        var argsParser = provider.GetRequiredService<IArgsParser>();
+        await argsParser.ParseAsyncLinear(args);
+    }
+}
