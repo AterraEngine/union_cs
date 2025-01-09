157047f8f3ea0905fa69f2a26ff823341747bdb5
Anna Sas <anna.sas.dev@gmail.com>
2025-01-09 17:27:07 +0100

    VersionBump : v3.6.0-preview.3

--- src/Tools.AterraEngine.Unions/Program.cs
+++ src/Tools.AterraEngine.Unions/Program.cs
@@ -24,6 +24,7 @@ public static class Program {
         //      Code is the true saviour
         string projects = string.Join(";",
             "AterraEngine.Unions",
+            "AterraEngine.Unions.Lib",
             "AterraEngine.Unions.Generators"
         );
 
